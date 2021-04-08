eeca51141f72cb11aa21faff81dbbfb93bad4801
smartdev0725 <cetbdb23@gmail.com>
2021-04-08 17:51:27 +0530

    fix: expect 0th account to have usdc instead of first

--- lemma_contracts/test/lemmaToken.js
+++ lemma_contracts/test/lemmaToken.js
@@ -12,7 +12,7 @@ contract("LemmaToken", accounts => {
   const testusdcAddress = "0xe0B887D54e71329318a036CF50f30Dbe4444563c";
   const uniswapV2Router02 = "0x1C232F01118CB8B424793ae03F870aa7D0ac7f77";
   const WETHAddress = "0x359eaF429cd6114c6fcb263dB04586Ad59177CAc";
-  
+
   let LemmaHoneySwapContract;
   let LemmaPerpetualContract;
   let uniswap;
@@ -26,36 +26,36 @@ contract("LemmaToken", accounts => {
     uniswap = new web3.eth.Contract(UNISWAP_ABI, "0x1C232F01118CB8B424793ae03F870aa7D0ac7f77");
   });
 
-  it("Deploy LemmaToken", async function() {
+  it("Deploy LemmaToken", async function () {
     LemmaTokenContract = await LemmaToken.new(testusdcAddress, WETHAddress, LemmaPerpetualContract.address, LemmaHoneySwapContract.address);
     assert.equal(await LemmaTokenContract.name(), "LemmaUSDT");
     assert.equal(await LemmaTokenContract.symbol(), "LUSDT");
   });
 
-  it("Set LemmaToken on LemmaPerpetual", async function() {
-    await LemmaPerpetualContract.setLemmaToken(LemmaTokenContract.address, {from: accounts[0]});
+  it("Set LemmaToken on LemmaPerpetual", async function () {
+    await LemmaPerpetualContract.setLemmaToken(LemmaTokenContract.address, { from: accounts[0] });
     assert.equal(await LemmaPerpetualContract.lemmaToken(), LemmaTokenContract.address);
   });
 
-  it("Set LemmaToken on LemmaHoneySwap", async function() {
-    await LemmaHoneySwapContract.setLemmaToken(LemmaTokenContract.address, {from: accounts[0]});
+  it("Set LemmaToken on LemmaHoneySwap", async function () {
+    await LemmaHoneySwapContract.setLemmaToken(LemmaTokenContract.address, { from: accounts[0] });
     assert.equal(await LemmaHoneySwapContract.lemmaToken(), LemmaTokenContract.address);
   });
 
-  it("Mint LemmaToken", async function() {
-    let  amountDeposit = web3.utils.toBN(1 * 10 ** 6);
-    let test_usdc_balance_1 = await testusdc.methods.balanceOf(accounts[1]).call();
-    await testusdc.methods.approve(LemmaTokenContract.address, amountDeposit).send({from:accounts[1], gas: 3000000, gasPrice: web3.utils.toWei("1", "gwei")});
-    await LemmaTokenContract.mint(amountDeposit, {from: accounts[1]});
-    let test_usdc_balance_1_afterMint = await testusdc.methods.balanceOf(accounts[1]).call();
+  it("Mint LemmaToken", async function () {
+    let amountDeposit = web3.utils.toBN(1 * 10 ** 6);
+    let test_usdc_balance_1 = await testusdc.methods.balanceOf(accounts[0]).call();
+    await testusdc.methods.approve(LemmaTokenContract.address, amountDeposit).send({ from: accounts[0], gas: 3000000, gasPrice: web3.utils.toWei("1", "gwei") });
+    await LemmaTokenContract.mint(amountDeposit, { from: accounts[0] });
+    let test_usdc_balance_1_afterMint = await testusdc.methods.balanceOf(accounts[0]).call();
     assert.equal(test_usdc_balance_1_afterMint, test_usdc_balance_1 - amountDeposit);
   });
 
-  it("Redeem LemmaToken", async function() {
+  it("Redeem LemmaToken", async function () {
     let amountRedeem = web3.utils.toBN(1 * 10 ** 18);
-    let test_usdc_balance_1 = await testusdc.methods.balanceOf(accounts[1]).call();
-    await LemmaTokenContract.redeem(amountRedeem, {from:  accounts[1]});
-    let test_usdc_balance_1_afterRedeem = await testusdc.methods.balanceOf(accounts[1]).call();
+    let test_usdc_balance_1 = await testusdc.methods.balanceOf(accounts[0]).call();
+    await LemmaTokenContract.redeem(amountRedeem, { from: accounts[0] });
+    let test_usdc_balance_1_afterRedeem = await testusdc.methods.balanceOf(accounts[0]).call();
     assert(test_usdc_balance_1_afterRedeem > test_usdc_balance_1);
   });
 });
